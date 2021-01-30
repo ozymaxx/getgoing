07be24dbf4049545bdb263a44024aae88843ee4e
Ozan Can Altiok <ozymaxx@gmail.com>
2021-01-30 21:48:02 +0100

    cover the nonascii chars in the tests

--- tries/Trie_test.go
+++ tries/Trie_test.go
@@ -9,6 +9,36 @@ func TestTrie(t *testing.T) {
 			t.Error("the empty word must exist in the trie")
 		}
 	})
+	t.Run("when 'çilek', 'çile' and 'çilekçi' are added, both must exist in the trie", func(t *testing.T) {
+		word1 := "çilek"
+		word2 := "çile"
+		word3 := "çilekçi"
+		if trie.ContainsWord(word1) || trie.ContainsWord(word2) || trie.ContainsWord(word3) {
+			t.Errorf("none of '%s', '%s' and '%s' must exist in the trie because they have not been added yet", word1, word2, word3)
+		}
+		trie.AddWord(word1)
+		if !trie.ContainsWord(word1) {
+			t.Errorf("the word '%s' must exist in the trie", word1)
+		}
+		if trie.ContainsWord(word2) || trie.ContainsWord(word3) {
+			t.Errorf("neither '%s' nor '%s' must exist in the trie because they have not been added yet", word2, word3)
+		}
+		trie.AddWord(word2)
+		if !trie.ContainsWord(word1) || !trie.ContainsWord(word2) {
+			t.Errorf("both '%s' and '%s' must exist in the trie", word1, word2)
+		}
+		if trie.ContainsWord(word3) {
+			t.Errorf("the word '%s' must not exist in the trie because it has not been added yet", word3)
+		}
+		trie.AddWord(word3)
+		if !trie.ContainsWord(word1) || !trie.ContainsWord(word2) || !trie.ContainsWord(word3) {
+			t.Errorf("'%s', '%s' and '%s' must exist in the trie", word1, word2, word3)
+		}
+		wordNotAdded := "cilek"
+		if trie.ContainsWord(wordNotAdded) {
+			t.Errorf("the word '%s' must not exist in the trie because it has not been added yet", wordNotAdded)
+		}
+	})
 	t.Run("when 'ozan' is added, both the empty word and 'ozan' must exist in the trie", func(t *testing.T) {
 		word := "ozan"
 		if trie.ContainsWord(word) {
@@ -40,7 +70,7 @@ func TestTrie(t *testing.T) {
 			t.Errorf("the word '%s' must not exist in the trie", anotherWord)
 		}
 	})
-	t.Run("when 'abc', 'abcd' and 'ab' is added in order, both must exist in the trie", func(t *testing.T) {
+	t.Run("when 'abc', 'abcd' and 'ab' is added in the given order, both must exist in the trie", func(t *testing.T) {
 		word1 := "abc"
 		word2 := "abcd"
 		word3 := "ab"
